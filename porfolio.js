d4249c76acbb3e7893c0127f8244d72c734a3fea
singhpriyanshuu <singh.priyanshuu12@gmail.com>
2026-01-19 01:13:32 +0530

    Initial portfolio website setup
    Created a personal portfolio website using HTML, CSS, and JavaScript.
    Added responsive navigation menu and basic interactivity.

---
+++ porfolio.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+  let menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
+function msg(){
+  alert("Thanks for visiting my portfolio!");
+}
